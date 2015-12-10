43d9ff1bc1af5e4c7c0cccb66dd8d225b6b84e1a
Jeremy Dyer <jdye64@gmail.com>
2015-12-10 08:25:06 -0500

    removed randerzander docker-ambari submodule and increase virtualbox VM CPU, MEM, and DISK

--- install_macosx.sh
+++ install_macosx.sh
@@ -1,6 +1,9 @@
 echo "Setting up docker-machine for docker-hwx project"
 VM_NAME="docker-hwx"
-DM_CMD="docker-machine create -d virtualbox $VM_NAME"
+CPU_COUNT=4
+DISK_SIZE_MB=80000
+MEM_MB=8024
+DM_CMD="docker-machine create -d virtualbox --virtualbox-disk-size $DISK_SIZE_MB --virtualbox-memory $MEM_MB --virtualbox-cpu-count $CPU_COUNT $VM_NAME"
 echo "Running $DM_CMD"
 $DM_CMD
 echo "Setting docker-machine active environment to $VM_NAME"
--- nifi/base/DockerBuild.sh
+++ nifi/base/DockerBuild.sh
@@ -1,2 +1,3 @@
 echo "Creating Docker image from the master branch at https://github.com/apache/nifi this is not a formal release so please use at your own risk"
+eval "$(docker-machine env docker-hwx)"
 docker build -t jdye64/nifi-snapshot .
