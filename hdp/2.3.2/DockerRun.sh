cbdf26f01c2fbdd96ae327f152979b0c589f9952
Jeremy Dyer <jdye64@gmail.com>
2015-12-22 14:28:39 -0500

    HDP 2.3.2 Support

---
+++ hdp/2.3.2/DockerRun.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+SLEEP_SEC="20"
+AMBARI_PORT="8080"
+HDP_IMAGE_NAME="jdye64/hdp232"
+
+DOCKER_MACHINE_NAME=env | grep docker | grep DOCKER_MACHINE_NAME | cut -f2 -d'='
+echo "Current Docker Machine '$DOCKER_MACHINE_NAME'"
+
+# Checks for an instance of $NIFI_IMAGE_NAME already running
+CONTAINER_ID=$(docker ps | grep nifi-latest | awk '{ print $1 }')
+if [ -n "$CONTAINER_ID" ]; then
+	echo "There is already an instance of $HDP_IMAGE_NAME running as container $CONTAINER_ID"
+	while true; do
+    	read -p "Would you like to kill the already running $HDP_IMAGE_NAME container?" yn
+    	case $yn in
+        	[Yy]* ) echo "killing Docker container $CONTAINER_ID"; docker kill $CONTAINER_ID; break;;
+        	[Nn]* ) exit;;
+        	* ) echo "Please answer yes or no.";;
+    	esac
+	done
+fi
+
+echo "Launching latest $HDP_IMAGE_NAME instance"
+CONTAINER_ID=$(docker run -t -d -P -h docker.dev $HDP_IMAGE_NAME)
+
+IP_ADDR=$(docker-machine inspect $DOCKER_MACHINE_NAME | grep IPAddress | cut -f2 -d':' | cut -f2 -d'"')
+echo "IPAddress: $IP_ADDR"
+AMBARI_URL="http://$IP_ADDR:$AMBARI_PORT"
+echo "Opening $HDP_IMAGE_NAME WebUI at $AMBARI_URL"
+echo "Sleeping for $SLEEP_SEC seconds before opening browser to give NiFi time to launch WebUI"
+sleep $SLEEP_SEC
+open $AMBARI_URL
