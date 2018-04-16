7e48331b354088696ba4e3aab5294547b54fc222
Jeremy Dyer <jdye64@gmail.com>
2018-04-16 12:53:26 -0400

    Add HDP 2.6.4

---
+++ hdp-repo/2.6.4/DockerBuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Creating Hortonworks Local Repo"
+docker build -t jdye64/docker-hwx:hdp-repo-2.6.4 .
---
+++ hdp-repo/2.6.4/DockerRun.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it -d -p 80:80 jdye64/docker-hwx:hdp-repo-2.6.4
