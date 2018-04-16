14f50b98f256f376e850589448339eb59b900bd9
Jeremy Dyer <jdye64@gmail.com>
2018-04-16 12:53:13 -0400

    Add HDF 3.1.1

---
+++ hdf/3.1.1/DockerBuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Creating HDF 3.1.1 Image" 
+cd ./base && ./DockerBuild.sh
+cd ../
+cd ./server && ./DockerBuild.sh
+cd ../
+cd ./agent && ./DockerBuild.sh
+cd ../
---
+++ hdf/3.1.1/base/DockerBuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Creating HDF 3.1.1 Base Docker Image" 
+docker build -t jdye64/docker-hwx:hdf-3.1.1-base .
